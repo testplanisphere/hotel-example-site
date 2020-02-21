593d341121e3ca6fecc74cd44feb47bbe09f8fbe
Takeshi Kishi <takeya0x86@gmail.com>
2020-02-21 23:01:43 +0900

    fix code style

--- docs/js/global.js
+++ docs/js/global.js
@@ -1,16 +1,16 @@
 export const PRESET_USER = new Map([
-  ["email", "ichiro@example.com"],
-  ["password", "password"],
+  ['email', 'ichiro@example.com'],
+  ['password', 'password'],
 ]);
 
 export function ready(handler) {
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", handler);
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', handler);
   } else {
     handler();
   }
 }
 
 export function isValidUser(email, password) {
-  return (PRESET_USER.get("email") === email && PRESET_USER.get("password") === password)
+  return (PRESET_USER.get('email') === email && PRESET_USER.get('password') === password);
 }
--- docs/js/login.js
+++ docs/js/login.js
@@ -1,24 +1,24 @@
-import { ready, isValidUser } from "./global.js";
+import { ready, isValidUser } from './global.js';
 
-ready(function() {
-  const form = document.getElementById("login-form");
-  form.addEventListener("submit", function(event) {
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
-    emailInput.setCustomValidity("");
-    passwordInput.setCustomValidity("");
+ready(() => {
+  const form = document.getElementById('login-form');
+  form.addEventListener('submit', (event) => {
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    emailInput.setCustomValidity('');
+    passwordInput.setCustomValidity('');
     if (emailInput.checkValidity() && passwordInput.checkValidity()) {
       if (!isValidUser(emailInput.value, passwordInput.value)) {
-        emailInput.setCustomValidity("メールアドレスまたはパスワードが違います。");
-        passwordInput.setCustomValidity("メールアドレスまたはパスワードが違います。");
+        emailInput.setCustomValidity('メールアドレスまたはパスワードが違います。');
+        passwordInput.setCustomValidity('メールアドレスまたはパスワードが違います。');
       }
     }
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
-      document.getElementById("email-message").textContent = emailInput.validationMessage;
-      document.getElementById("password-message").textContent = passwordInput.validationMessage;
-      form.classList.add("was-validated");
+      document.getElementById('email-message').textContent = emailInput.validationMessage;
+      document.getElementById('password-message').textContent = passwordInput.validationMessage;
+      form.classList.add('was-validated');
     }
   });
 });
