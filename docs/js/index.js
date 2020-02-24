95a9de236f00c7c31db5aebf7d7863b9c3577b1b
Takeshi Kishi <takeya0x86@gmail.com>
2020-02-25 02:45:05 +0900

    fix session manage

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,19 +2,12 @@ import { ready, getSessionUser, logout } from './global.js';
 
 ready(() => {
   const session = getSessionUser();
-  if (session === '') {
-    document.getElementById('signup-holder')
-        .insertAdjacentHTML('afterbegin', '<a class="nav-link" href="./signup.html">会員登録</a>');
-    document.getElementById('login-holder')
-        .insertAdjacentHTML('afterbegin', '<a class="btn btn-outline-secondary my-2 my-sm-0" href="./login.html" role="button">ログイン</a>');
-  } else {
-    document.getElementById('signup-holder')
-        .insertAdjacentHTML('afterbegin', '<a class="nav-link" href="./mypage.html">マイページ');
-    document.getElementById('login-holder')
-        .insertAdjacentHTML('afterbegin', '<form action="./index.html" class="form-inline" id="logout-form"><button type="submit" class="btn btn-outline-success my-2 my-sm-0">ログアウト</button></form>');
+  if (session !== '') {
+    document.getElementById('signup-holder').innerHTML = '<a class="nav-link" href="./mypage.html">マイページ</a>';
+    document.getElementById('login-holder').innerHTML
+        = '<form action="./index.html" class="form-inline" id="logout-form"><button type="submit" class="btn btn-outline-success my-2 my-sm-0">ログアウト</button></form>';
+    document.getElementById('logout-form').addEventListener('submit', (event) => {
+      logout();
+    });
   }
-
-  document.getElementById('logout-form').addEventListener('submit', (event) => {
-    logout();
-  });
 });
