dba72a706548969edfbc6631b53d0514bc2e9a92
Takeshi Kishi <takeya0x86@gmail.com>
2020-03-07 06:03:03 +0900

    set Login state

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,14 +1,9 @@
 import { ready } from './global.js';
-import { getSessionUser, logout } from './session.js';
+import { getSessionUser, setLoginNavbar } from './session.js';
 
 ready(() => {
   const session = getSessionUser();
   if (session) {
-    document.getElementById('signup-holder').innerHTML = '<a class="nav-link" href="./mypage.html">マイページ</a>';
-    document.getElementById('login-holder').innerHTML
-        = '<form action="./index.html" class="form-inline" id="logout-form" novalidate><button type="submit" class="btn btn-outline-success my-2 my-sm-0">ログアウト</button></form>';
-    document.getElementById('logout-form').addEventListener('submit', (event) => {
-      logout();
-    });
+    setLoginNavbar();
   }
 });
--- docs/js/session.js
+++ docs/js/session.js
@@ -89,3 +89,12 @@ export function login(email) {
 export function logout() {
   document.cookie = 'session=; max-age=0';
 }
+
+export function setLoginNavbar() {
+  document.getElementById('signup-holder').innerHTML = '<a class="nav-link" href="./mypage.html">マイページ</a>';
+  document.getElementById('login-holder').innerHTML
+      = '<form action="./index.html" class="form-inline" id="logout-form" novalidate><button type="submit" class="btn btn-outline-success my-2 my-sm-0">ログアウト</button></form>';
+  document.getElementById('logout-form').addEventListener('submit', (event) => {
+    logout();
+  });
+}
