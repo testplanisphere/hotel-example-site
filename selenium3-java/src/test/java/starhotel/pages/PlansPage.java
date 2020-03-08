1012de9dfc83845816938f1baa211b749d25680a
Takeshi Kishi <takeya0x86@gmail.com>
2020-03-08 16:52:38 +0900

    add waiting load

--- selenium3-java/src/test/java/starhotel/pages/PlansPage.java
+++ selenium3-java/src/test/java/starhotel/pages/PlansPage.java
@@ -1,23 +1,28 @@
 package starhotel.pages;
 
-import static java.util.stream.Collectors.toList;
-
 import java.util.List;
+import java.util.stream.Collectors;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PlansPage {
 
   private WebDriver driver;
 
+  private WebDriverWait wait;
+
   public PlansPage(WebDriver driver) {
     this.driver = driver;
+    this.wait = new WebDriverWait(driver, 30);
   }
 
   public List<String> getPlanTitles() {
+    wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("#plan-list > div[role=\"status\"]"), 0));
     var plans = driver.findElements(By.className("card-title"));
-    return plans.stream().map(WebElement::getText).collect(toList());
+    return plans.stream().map(WebElement::getText).collect(Collectors.toList());
   }
 
 
